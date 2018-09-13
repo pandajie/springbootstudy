7236afa5dc558116cadf60c027813cd6d3a1a7b7
zhaojie <zhaojie@DESKTOP-1M2OPL5>
2018-09-14 00:44:57 +0800

    add spring boot

--- demo/src/main/java/com/zj/demo/controller/HelloController.java
+++ demo/src/main/java/com/zj/demo/controller/HelloController.java
@@ -1,7 +1,14 @@
 package com.zj.demo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/index")
 public class HelloController {
-	public static void main(String[] args) {
-		System.out.print("hello");
+	
+	@RequestMapping("hello")
+	public String hello() {
+		return "hello";
 	}
 }
